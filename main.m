0f520f45ff1f688b7cf53474e9dc74a9e83c81d2
Travis Nesland <tnesland@gmail.com>
2010-08-29 12:04:02 -0400

    Initial commit.

---
+++ main.m
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////
+//  main.m
+//  TK-Session
+//  --------------------------------------------------------
+//  Author: Travis Nesland <tnesland@gmail.com>
+//  Created: 8/29/10
+//  Copyright Residential Research Facility, University of
+//  Kentucky, 2010. All rights reserved.
+/////////////////////////////////////////////////////////////
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
