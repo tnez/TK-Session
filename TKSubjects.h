b2b39421cb9c9da79df5e6b66a512ca5310a214d
Travis Nesland <tnesland@gmail.com>
2010-10-28 16:31:38 -0400

    Clock out 2010-10-28-16-31

--- TKSubjects.h
+++ TKSubjects.h
@@ -11,24 +11,28 @@
 #import <Cocoa/Cocoa.h>
 
 @interface TKSubjects : NSObject {
+
 	NSMutableArray *subjects;
+
 }
+
 @property (retain) NSMutableArray *subjects;
--(void) add;
--(void) clear;
--(void) clearDataForKey:(NSString *) key;
--(NSInteger) count;
--(NSMutableDictionary *) objectAtIndex:(NSInteger) index;
--(void) readSubjectsFromFile: (NSString *)pathToSubjectsFile;
--(void) removeSubjects:(NSIndexSet *) index;
--(void) sortUsingDescriptors:(NSArray *) newDescriptors;
--(void) writeSubjectsToFile: (NSString *)pathToSubjectsFile;
+
+- (void)add;
+- (void)clear;
+- (void)clearDataForKey: (NSString *)key;
+- (NSInteger)count;
+- (NSMutableDictionary *)objectAtIndex: (NSInteger)index;
+- (void)readSubjectsFromFile: (NSString *)pathToSubjectsFile;
+- (void)removeSubjects: (NSIndexSet *)index;
+- (void)sortUsingDescriptors: (NSArray *)newDescriptors;
+- (void)writeSubjectsToFile: (NSString *)pathToSubjectsFile;
 
 #pragma mark Table View Data Source Protocol
--(NSInteger) numberOfRowsInTableView:(NSTableView *) table;
--(void) tableView:(NSTableView *) table sortDescriptorsDidChange:(NSArray *) oldDescriptors;
--(void) tableView:(NSTableView *) table setObjectValue:(id) newObject forTableColumn:(NSTableColumn *) column row:(NSInteger) row;
--(id) tableView:(NSTableView *) table objectValueForTableColumn:(NSTableColumn *) column row:(NSInteger) row;
+- (NSInteger)numberOfRowsInTableView: (NSTableView *)table;
+- (void)tableView: (NSTableView *)table sortDescriptorsDidChange: (NSArray *)oldDescriptors;
+- (void)tableView: (NSTableView *)table setObjectValue: (id)newObject forTableColumn: (NSTableColumn *)column row: (NSInteger)row;
+- (id)tableView: (NSTableView *)table objectValueForTableColumn: (NSTableColumn *)column row: (NSInteger)row;
 
 @end
 
